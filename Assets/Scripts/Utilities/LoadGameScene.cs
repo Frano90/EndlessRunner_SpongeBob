fb0698b14552cee9566fcea863eec3a5ed50c5b6
Francisco Criscuolo <fran0w@Franciscos-MacBook-Pro.local>
2020-01-31 12:16:18 -0300

    Agregada scena intro

---
+++ Assets/Scripts/Utilities/LoadGameScene.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadGameScene : MonoBehaviour
+{
+    [SerializeField] private Button _startButon;
+    [SerializeField] private Text _text;
+
+    private void Awake()
+    {
+        _startButon.onClick.AddListener(LoadScene);
+    }
+
+    private void LoadScene()
+    {
+        StartCoroutine(LoadYourAsyncScene("Race"));
+        _text.gameObject.SetActive(true);
+    }
+    
+    public IEnumerator LoadYourAsyncScene(string sceneName)
+    {
+        
+        AsyncOperation asyncLoad = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            Debug.Log("Empiezo a cargar");
+            yield return null;
+        }
+        
+        Debug.Log("Termine de cargar el juego");
+        
+        asyncLoad.allowSceneActivation = true;
+    }
+}
